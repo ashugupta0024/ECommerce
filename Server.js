bf9d95d349097d72490f01926f1c5c2b04177eaa
Ayush Gupta <96935509+ashugupta0024@users.noreply.github.com>
2023-07-06 20:50:58 +0530

    Edited

--- Server.js
+++ Server.js
@@ -7,15 +7,9 @@ import authRoute from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/CategoryRoutes.js";
 import productRoutes from "./routes/ProductRoutes.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
-
-//esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -27,22 +21,15 @@ connectDB();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/product", productRoutes)
 
 //rest api
-
-// app.get("/", (req, res) => {
-// 	res.send("<h1> Welcome to E-Commerce MERN Stack APP </h1>");
-// });
-
-//Deployment
-app.use("*", function (req, res) {
-	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/", (req, res) => {
+	res.send("<h1> Welcome to E-Commerce MERN Stack APP </h1>");
 });
 
 //PORT
