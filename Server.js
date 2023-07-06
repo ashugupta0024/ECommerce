c333a38092a44a95235235ca7fed3305cba9d616
Ayush Gupta <96935509+ashugupta0024@users.noreply.github.com>
2023-07-06 20:12:39 +0530

    esModule fix

--- Server.js
+++ Server.js
@@ -8,9 +8,14 @@ import cors from "cors";
 import categoryRoutes from "./routes/CategoryRoutes.js";
 import productRoutes from "./routes/ProductRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
